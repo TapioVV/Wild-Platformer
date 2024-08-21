70bcc3315df362287039b9279d6a9de94ad95219
Tapio Vähä-Vahe <102945614+TapioVV@users.noreply.github.com>
2024-08-21 16:20:33 +0300

    asdasdasd

--- WildPlatformer/Assets/Scripts/Gun.cs
+++ WildPlatformer/Assets/Scripts/Gun.cs
@@ -40,4 +40,60 @@ public class Gun : MonoBehaviour
             shootingActivated = false;
         }
     }
+
+    /*[SerializeField] GameObject bulletPrefab;
+    AudioSource audioSource;
+    [SerializeField] AudioClip ShootSound;
+    float shootingTimer;
+    [SerializeField] float shootingCooldown;
+    bool shootingActivated = false;
+    [SerializeField] Transform gunPivotTransform;
+    [SerializeField] Transform playerTransform;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        shootingTimer = Mathf.MoveTowards(shootingTimer, -1, Time.deltaTime);
+
+        if (shootingActivated == true)
+        {
+            if (shootingTimer <= 0)
+            {
+                audioSource.PlayOneShot(ShootSound);
+                Instantiate(bulletPrefab, transform.position, transform.rotation);
+                shootingTimer = shootingCooldown;
+            }
+        }
+    }
+    public void ShootInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            shootingActivated = true;
+        }
+        if (context.canceled)
+        {
+            shootingActivated = false;
+        }
+    }
+    public void MouseAim(InputAction.CallbackContext context)
+    {
+
+        Debug.Log("Jee");
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+
+        float angle = Mathf.Atan2(playerTransform.position.y - mousePosition.y, playerTransform.position.x - mousePosition.x) * Mathf.Rad2Deg + 90;
+        gunPivotTransform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+    public void ControllerAim(InputAction.CallbackContext context)
+    {
+        if (!context.canceled && context.ReadValue<Vector2>() != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(context.ReadValue<Vector2>().y, context.ReadValue<Vector2>().x) * Mathf.Rad2Deg - 90;
+            gunPivotTransform.localRotation = Quaternion.Euler(0, 0, angle);
+        }
+    }*/
 }
